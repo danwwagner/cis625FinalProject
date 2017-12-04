b1ee07e965b66f9c9787dd10f2e12ac43e12e3cc
Dan Wagner <danwagner@ksu.edu>
2017-12-04 12:40:00 -0600

    Modified header file for pointers and parameters.

--- src/Genetics.h
+++ src/Genetics.h
@@ -1,4 +1,7 @@
 #include "Individual.h"
+#include <string>
+
+using std::string;
 
 class Genetics
 {
@@ -9,7 +12,7 @@ class Genetics
 		Genetics(float, float, int, int);
 		~Genetics()
 
-		void Crossover(Individual, Individual); // Crossover of two individuals
-		void Mutate(Individual); // Mutation of a single individual
+		void Crossover(Individual&, Individual&); // Crossover of two individuals
+		void Mutate(Individual&, string); // Mutation of a single individual
 		Individual TournamentSelection(Individual*, int); // Tournament selection of individuals
 };
